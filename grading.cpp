ef1e8bdb7780819c1576f2787013e7feca1a9025
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-16 03:33:12 +0530

    display function

--- grading.cpp
+++ grading.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
 using namespace std;
 
+  
+
+void displayGrade(int mark)
+{
+    string grade = calculateGrade(mark);
+    cout<<"\nThe grade has been calculated successfully.\n";
+    cout<<"\nThe grade for the mark "<<mark<<" is: "<<grade<<"\n";
+}
+
 void initialisation()
 {
     cout<<"\nEnter the mark of the student: \n";
     int mark;
     cin>>mark;
     if(mark < 0 || mark > 100) {
-        cout<<"Invalid mark entered. Please enter a mark between 0 and 100.\n";
+        cout<<"\nInvalid mark entered. Please enter a mark between 0 and 100.\n\n";
         initialisation(); 
+    }
+    else
+    {
+        displayGrade(mark);
     }
 }
 
